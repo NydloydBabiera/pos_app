cd38d5b40e5ad1b0b6d9583773616d420a3e5062
NydloydBabiera <nydloyd.babiera2998@gmail.com>
2023-11-02 15:18:36 +0800

    added payment modal

--- cashier.js
+++ cashier.js
@@ -8,36 +8,41 @@ const addUser = document.getElementById("addUser");
 var selectedId = 0;
 let transactionId = 0;
 const socketCashier = new WebSocket("ws://localhost:3000");
+// const closeModalBtn = document.getElementById("cancel");
+const processModalbtn = document.getElementById("process");
+const totalTxtLbl = document.getElementById("totalTxt");
+const changeTxt = document.getElementById("changeTxt");
+let cashierId = 0;
 
-socketCashier.addEventListener("message", (event) => {
-  console.log("new data:", event.data.transaction_id);
-  // console.log("received message");
-  //  socketCashier.send("cashier",5000);
-});
+// socketCashier.addEventListener("message", (event) => {
+//   console.log("new data:", event.data.transaction_id);
+//   // console.log("received message");
+//   //  socketCashier.send("cashier",5000);
+// });
 
-socketCashier.onopen = (event) => {
-  console.log("WebsocketCashier connection opened:", event);
-  socketCashier.send("cashier");
-  // socketCashier.send("cashier", 5000);
-};
+// socketCashier.onopen = (event) => {
+//   console.log("WebsocketCashier connection opened:", event);
+//   socketCashier.send("cashier");
+//   // socketCashier.send("cashier", 5000);
+// };
 
-socketCashier.onmessage = (event) => {
-  console.log("received CASHIER");
-  const message = event.data;
-  // appendMessage(message);
-};
+// socketCashier.onmessage = (event) => {
+//   console.log("received CASHIER");
+//   const message = event.data;
+//   // appendMessage(message);
+// };
 
-socketCashier.onclose = (event) => {
-  if (event.wasClean) {
-    console.log("WebsocketCashier closed cleanly:", event);
-  } else {
-    console.error("WebsocketCashier connection closed unexpectedly:", event);
-  }
-};
+// socketCashier.onclose = (event) => {
+//   if (event.wasClean) {
+//     console.log("WebsocketCashier closed cleanly:", event);
+//   } else {
+//     console.error("WebsocketCashier connection closed unexpectedly:", event);
+//   }
+// };
 
-socketCashier.onerror = (error) => {
-  console.error("WebsocketCashier error:", error);
-};
+// socketCashier.onerror = (error) => {
+//   console.error("WebsocketCashier error:", error);
+// };
 
 function appendMessage(message) {
   const item = document.createElement("li");
@@ -74,7 +79,7 @@ async function fetchData() {
                               <td>${item.transaction_status}</td>
                               <td>${formattedDate}</td>
                               <td>${item.amt_total}</td>
-                              <td><button onClick="onEdit(this)">PAY</button>
+                              <td><button onClick="onEdit(this)">PAY</button></td>
                               
                           `;
         tableBody.appendChild(row);
@@ -87,43 +92,75 @@ async function fetchData() {
 
 // // Edit the data
 function onEdit(button) {
+  modalTitle.textContent = "Payment";
   var table = document.getElementById("apiTable");
+  document.getElementById("pmtAmt").value = ''
   // Get the parent row of the button (the <tr> element)
   const row = button.closest("tr");
 
   // Get the cell containing the price (2nd cell, index 1)
   transactionId = row.cells[0].textContent;
   const transactionCode = row.cells[1].textContent;
-
-
-  if (confirm(`Process transaction ${transactionCode}?`)) {
-    console.log("yes");
-    processTransaction();
+  const transactionStatus = row.cells[2].textContent;
+  totalTxtLbl.textContent = row.cells[4].textContent;
+  const isPaid = transactionStatus == "PR" ? true : false;
+  if (isPaid) {
+    alert("Transaction is already processed!");
     return;
   }
-}
-// add modal para sa pagdawat sa payment
-async function processTransaction() {
-  await fetch(`${apiUrl}/transaction/processTransaction/${transactionId}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-    },
-  })
-    .then((response) => response.json())
-    .then((result) => {
-      alert(result.message);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
+
+  modal.style.display = "block";
+  // if (confirm(`Process transaction ${transactionCode}?`)) {
+  //   console.log("yes");
+  //   processTransaction();
+  //   return;
+  // }
 }
 
+// Function to close the modal
+function closeModal() {
+  modal.style.display = "none";
+}
+
+// add modal para sa pagdawat sa payment
+document
+  .getElementById("processForm")
+  .addEventListener("submit", function (event) {
+    const txtPmtAmt = document.getElementById("pmtAmt").value;
+    event.preventDefault(); // Prevent the default form submission
+    const data = {
+      transaction_id: transactionId,
+      cashier_id: cashierId,
+      amt_paid: Number(txtPmtAmt),
+    };
+    if (txtPmtAmt < Number(totalTxtLbl.textContent)) {
+      alert("Payment received is insufficient!");
+      return;
+    }
+    fetch(`${apiUrl}/transaction/processTransaction`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        alert(result.message);
+        changeTxt.textContent =  Number(txtPmtAmt) - Number(totalTxtLbl.textContent)
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
 window.addEventListener("load", function () {
+  cashierId = this.localStorage.getItem("userId");
   if (!this.localStorage.getItem("userId")) {
     window.location.href = "index.html";
   }
   fetchData();
-  console.log("");
+  console.log("cashierId:", cashierId);
 });
+
+closeModalButton.addEventListener("click", closeModal);
